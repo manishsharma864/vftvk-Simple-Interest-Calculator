283f43342c22f3786f6879dd4ecbe527537bece1
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2021-08-24 18:28:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function compute()
     p = document.getElementById("principal").value;
     r = document.getElementById("rate").value;
     t = document.getElementById("years").value;
+    if(p<=0){
+       alert("Enter the positive no.");
+    }
+        
 s=p*r*t;
 u= new Date();
 h= parseInt(u.getFullYear());
