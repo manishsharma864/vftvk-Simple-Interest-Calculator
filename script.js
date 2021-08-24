79ca992bf533ba233fb8c3591e9b40516e6c2f2a
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2021-08-24 18:19:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+s=p*r*t;
+u= new Date();
+h= parseInt(u.getFullYear());
+n=parseInt(t)
+d=h+n;
+document.getElementById("result").innerHTML = "if you deposit <mark>"+p+"</mark>,at an interest rate of<mark> "+r+"</mark>You will recieve amount of <mark>"+s+"</mark> in year <mark>"+d+"</mark>";
     
 }
-        
